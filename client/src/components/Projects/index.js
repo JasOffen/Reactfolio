f89ae6bcbd7fb283ec9a02a9923907df6d9f37cb
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-08-11 18:31:25 -0700

    mapped data out and changed the GET request for sorting and limitation

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -1,38 +1,58 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 function Projects() {
 
-    const gitHubURL = "https://api.github.com/users/JasOffen/repos"
-    const [repos, setRepos ] = useState([])
-    const [repoLoading, setLoading ] = useState([])
+    const gitHubURL = "https://api.github.com/users/JasOffen/repos?sort=updated&direction=des&per_page=5"
+    const [repos, setRepos] = useState([])
+    const [repoLoading, setLoading] = useState([])
     const [userData, setUserData] = useState([])
     const [userLoading, setUserLoading] = useState([])
     // get GitHub Repo data
     const getGitRepos = async () => {
         const response = await fetch(gitHubURL)
         const json = await response.json()
-        setRepos(json)
-        if(response.ok){
-            setLoading(response)
+        try {
+            setRepos(json)
+            if (response.ok) {
+                setLoading(response)
+            }
+        } catch {
+            console.log(`idk man there was an error!`)
         }
+        console.log(json)
     }
 
-    useEffect(() => { getGitRepos()}, [])
-        // Check if we're still loading data
-        if(!repoLoading){return(<div>Loading...</div>)}else{
+    useEffect(() => { getGitRepos() }, [])
+    // Check if we're still loading data
+    if (!repoLoading) { return (<div uk-spinner="ratio: 3"></div>) } else {
         return (
             <section id="Project-Cards">
-                {repos.map((data) => {
-                    return(
-                    <div key = {data.id} class="uk-card uk-card-body">
-                        <a href={data.html_url} target="_blank" rel="noreferrer">{data.name}</a>
-                    </div>
-                    )
-                })}
+                <div className="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid="">
+                    {repos.map((data) => {
+                        return (
+                            <div key={data.id}>
+                                <div className="uk-card-header">
+                                    <div className="uk-width-expand">
+                                        <h3 className="uk-card-title uk-margin-remove-bottom">{data.name}</h3>
+                                        {/* format date here to be easier to read */}
+                                        <p className="uk-text-meta uk-margin-remove-top"><time dateTime="2016-04-01T19:00">{data.updated_at}</time></p>
+                                    </div>
+                                </div>
+                                <div className="uk-card-body">
+                                    <p>{data.description}</p>
+                                </div>
+                                <div className="uk-card-footer">
+                                    <a href="#" class="uk-button uk-button-text">Read more</a>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </section>
         )
-            }
+    }
 }
 
 export default Projects
